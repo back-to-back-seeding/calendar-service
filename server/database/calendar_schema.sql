1dbe1300d966dfb277ad66ebce8f081b824bd154
Blanca Gomez <blancagoand@gmail.com>
2020-08-29 14:03:51 -0700

    generate rooms and users

--- server/database/calendar_schema.sql
+++ server/database/calendar_schema.sql
@@ -8,8 +8,8 @@ CREATE TABLE prices (
     from_date date  NOT NULL,
     to_date date  NOT NULL,
     price int  NOT NULL,
-    room_id int  NOT NULL,
-    CONSTRAINT id PRIMARY KEY (id)
+    room_id int  NOT NULL
+    -- CONSTRAINT id PRIMARY KEY (id)
 );
 
 -- Table: reservations
@@ -18,8 +18,8 @@ CREATE TABLE reservations (
     check_in date  NOT NULL,
     check_out date  NOT NULL,
     room_id int  NOT NULL,
-    user_id int  NOT NULL,
-    CONSTRAINT reservations_pk PRIMARY KEY (id)
+    user_id int  NOT NULL
+    -- CONSTRAINT reservations_pk PRIMARY KEY (id)
 );
 
 -- Table: rooms
@@ -28,8 +28,8 @@ CREATE TABLE rooms (
     minimum_stay int  NOT NULL,
     maximum_guest int  NOT NULL,
     reviews int  NULL,
-    rating DECIMAL(3,2) NULL,
-    CONSTRAINT rooms_pk PRIMARY KEY (id)
+    rating DECIMAL(3,2) NULL
+    -- CONSTRAINT rooms_pk PRIMARY KEY (id)
 );
 
 -- Table: users
@@ -38,34 +38,34 @@ CREATE TABLE users (
     first_name varchar(80)  NOT NULL,
     last_name varchar(80)  NOT NULL,
     email varchar(20)  NOT NULL,
-    password varchar(20)  NOT NULL,
-    CONSTRAINT users_pk PRIMARY KEY (id)
+    password varchar(20)  NOT NULL
+    -- CONSTRAINT users_pk PRIMARY KEY (id)
 );
 
 -- foreign keys
 -- Reference: Pricing_room (table: prices)
-ALTER TABLE prices ADD CONSTRAINT Pricing_room
-    FOREIGN KEY (room_id)
-    REFERENCES rooms (id)
-    NOT DEFERRABLE
-    INITIALLY IMMEDIATE
-;
+-- ALTER TABLE prices ADD CONSTRAINT Pricing_room
+--     FOREIGN KEY (room_id)
+--     REFERENCES rooms (id)
+--     NOT DEFERRABLE
+--     INITIALLY IMMEDIATE
+-- ;
 
--- Reference: reservation_room (table: reservations)
-ALTER TABLE reservations ADD CONSTRAINT reservation_room
-    FOREIGN KEY (room_id)
-    REFERENCES rooms (id)
-    NOT DEFERRABLE
-    INITIALLY IMMEDIATE
-;
+-- -- Reference: reservation_room (table: reservations)
+-- ALTER TABLE reservations ADD CONSTRAINT reservation_room
+--     FOREIGN KEY (room_id)
+--     REFERENCES rooms (id)
+--     NOT DEFERRABLE
+--     INITIALLY IMMEDIATE
+-- ;
 
--- Reference: reservation_user (table: reservations)
-ALTER TABLE reservations ADD CONSTRAINT reservation_user
-    FOREIGN KEY (user_id)
-    REFERENCES users (id)
-    NOT DEFERRABLE
-    INITIALLY IMMEDIATE
-;
+-- -- Reference: reservation_user (table: reservations)
+-- ALTER TABLE reservations ADD CONSTRAINT reservation_user
+--     FOREIGN KEY (user_id)
+--     REFERENCES users (id)
+--     NOT DEFERRABLE
+--     INITIALLY IMMEDIATE
+-- ;
 
 -- End of file.
 
